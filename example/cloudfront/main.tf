2fab357aee8f7e5747074f16707eb5b2d8405391
Derrick Petzold <github@bzero.io>
2018-02-08 20:14:27 -0800

    ...

--- example/cloudfront/main.tf
+++ example/cloudfront/main.tf
@@ -1,5 +1,5 @@
 module "lambda" {
-  source             = "../cloudfront"
+  source             = "../../cloudfront"
   domain_names       = "${var.domain_names}"
   stage_name         = "${var.stage_name}"
   origin_domain_name = "${var.origin_domain_name}"
--- example/vpc/main.tf
+++ example/vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source           = "..//vpc"
+  source           = "../../vpc"
   name             = "${var.project_name}"
   aws_key_name     = "${var.project_name}"
   aws_key_location = "${file(var.aws_key_location)}"
