1e8f29ce89f788213783cd604a5124553c701634
Derrick Petzold <github@bzero.io>
2018-02-08 20:20:01 -0800

    ...

--- example/cloudfront/main.tf
+++ example/cloudfront/main.tf
@@ -1,5 +1,5 @@
 module "lambda" {
-  source             = "../../cloudfront"
+  source             = "github.com/dpetzold/terraform-aws-zappa/cloudfront"
   domain_names       = "${var.domain_names}"
   stage_name         = "${var.stage_name}"
   origin_domain_name = "${var.origin_domain_name}"
