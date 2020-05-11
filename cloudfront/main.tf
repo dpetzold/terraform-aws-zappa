2406b0dfed8bb7f90a62a14945b0c5276ea009c1
Derrick Petzold <github@petzold.io>
2020-05-10 21:42:25 -0700

    updates for v12

--- cloudfront/main.tf
+++ cloudfront/main.tf
@@ -1,30 +1,54 @@
-data "aws_acm_certificate" "cert" {
-  count    = "${length(var.domain_names)}"
-  domain   = "${element(var.domain_names, count.index)}"
-  statuses = ["ISSUED"]
+variable stage_name {
+  description = "The name of the deployment stage"
 }
 
-resource "aws_cloudfront_distribution" "cf_distribution" {
-  count           = "${length(var.domain_names)}"
+variable api_name {
+  description = "The id of the api gateway"
+}
+
+variable domain_names {
+  description = "The name of the domain"
+  type        = list
+}
+
+variable wildcard_domains {
+  description = "The wildcards domains"
+  type        = list
+}
+
+variable www_domains {
+  description = "The www domains"
+  type        = list
+}
+
+variable cert_arn { }
+
+variable validation_record {}
+
+locals {
+  wildcards_and_domains = concat(var.domain_names, var.wildcard_domains)
+}
+
+resource aws_cloudfront_distribution cf_distribution {
   enabled         = true
   is_ipv6_enabled = true
   price_class     = "PriceClass_100"
-  aliases         = ["${element(var.domain_names, count.index)}"]
+  aliases         = concat(var.domain_names, var.wwww_names)
 
   origin {
-    domain_name = "${aws_api_gateway_domain_name.api.domain_name}"
+    domain_name = aws_api_gateway_domain_name.api.domain_name
     origin_id   = "lambda"
 
-    custom_origin_config = {
+    custom_header {
+      name  = "X-Forwarded-Host"
+      value = "petzold.io"
+    }
+
+    custom_origin_config {
       http_port              = 80
       https_port             = 443
       origin_protocol_policy = "https-only"
       origin_ssl_protocols   = ["TLSv1.2"]
-    }
-
-    custom_header = {
-      name  = "X-Forwarded-Host"
-      value = "${element(var.domain_names, count.index)}"
     }
   }
 
@@ -57,32 +81,32 @@ resource "aws_cloudfront_distribution" "cf_distribution" {
     max_ttl                = 86400
   }
 
-  custom_error_response = {
+  custom_error_response {
     error_code            = 403
     error_caching_min_ttl = 0
   }
 
-  custom_error_response = {
+  custom_error_response {
     error_code            = 500
     error_caching_min_ttl = 0
   }
 
-  custom_error_response = {
+  custom_error_response {
     error_code            = 501
     error_caching_min_ttl = 0
   }
 
-  custom_error_response = {
+  custom_error_response {
     error_code            = 502
     error_caching_min_ttl = 0
   }
 
-  custom_error_response = {
+  custom_error_response {
     error_code            = 503
     error_caching_min_ttl = 0
   }
 
-  custom_error_response = {
+  custom_error_response {
     error_code            = 504
     error_caching_min_ttl = 0
   }
@@ -94,23 +118,23 @@ resource "aws_cloudfront_distribution" "cf_distribution" {
     }
   }
 
-  tags {
-    Environment = "production"
-  }
-
   viewer_certificate {
-    acm_certificate_arn = "${element(data.aws_acm_certificate.cert.*.arn, count.index)}"
+    acm_certificate_arn = var.cert_arn
     ssl_support_method  = "sni-only"
   }
 }
 
-resource "aws_api_gateway_domain_name" "api" {
+resource aws_api_gateway_domain_name api {
   domain_name     = "api.${element(var.domain_names, 0)}"
-  certificate_arn = "${element(data.aws_acm_certificate.cert.*.arn, 0)}"
+  certificate_arn = var.cert_arn
 }
 
-resource "aws_api_gateway_base_path_mapping" "api_mapping" {
-  api_id      = "${var.api_id}"
-  stage_name  = "${var.stage_name}"
-  domain_name = "${aws_api_gateway_domain_name.api.domain_name}"
+data aws_api_gateway_rest_api rest_api {
+  name = var.api_name
+}
+
+resource aws_api_gateway_base_path_mapping api_mapping {
+  api_id      = data.aws_api_gateway_rest_api.rest_api.id
+  stage_name  = var.stage_name
+  domain_name = aws_api_gateway_domain_name.api.domain_name
 }
